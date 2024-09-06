af8667f4f04c1b26f677bc1215e2e0fbdfe81d8c
igorc <igorcastilhos2010@hotmail.com>
2024-09-05 23:50:23 -0300

    Add: Null

---
+++ src/main/Null.kt
@@ -0,0 +1,10 @@
+package main
+
+fun main() {
+    var name = "Igor"
+    println(name.uppercase())
+
+    var brand: String? = "Novaware"
+    println(brand?.uppercase())
+    println(brand?.lowercase())
+}
